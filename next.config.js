3bc88efab4851f86175db876708c9b87e5277630
Juan Treminio <juantreminio@dropbox.com>
2023-11-02 13:50:59 -0500

    Working example

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    webpack: (config, { isServer }) => {
+
+  // If client-side, don't polyfill `fs`
+  if (!isServer) {
+    config.resolve.fallback = {
+      fs: false,
+      axios: false,
+    };
+  }
+
+  return config;
+},
+
+};
